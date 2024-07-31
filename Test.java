62c209c2dd3f367202b92a4e7c4bc335b5b64c73
ElangDipa <elang_purwadhiyaksa_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:59:26 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Nama   = Elang");
+        System.out.println("Alamat = Bumi");
+        System.out.println("SMP    = ArrohmahPutra");
+        System.out.println("No HP  = 085933663806");
+    }
+}
